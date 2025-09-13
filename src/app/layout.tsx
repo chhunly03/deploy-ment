3bb923517358dbf5eba5bba9428cf1e29155a2ef
Chhunly <chhunly03@example.com>
2025-09-13 13:21:41 +0700

    Calendar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Khmer Calendar",
+  title: "Khmers Calendar",
   description: "Cambodian Calendar Application",
 };
 
