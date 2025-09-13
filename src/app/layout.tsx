be4b42221eed56923a68ce22194e68210f2f852e
Chhunly <chhunly03@example.com>
2025-09-13 17:19:18 +0700

    deploy calendar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "KHMEER CALENDAR",
+  title: "KHMEER CALENDARS",
   description: "Cambodian Calendar Application",
 };
 
