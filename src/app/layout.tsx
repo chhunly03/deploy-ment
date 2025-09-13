4623c5ab7631926f0ed17b91255ee847b045ac42
Chhunly <chhunly03@example.com>
2025-09-13 17:04:37 +0700

    deploy calendar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "koko Calendar",
+  title: "Khmer Calendar",
   description: "Cambodian Calendar Application",
 };
 
